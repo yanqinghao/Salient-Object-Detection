3e24c59f21ac7b86d4dde8b27b49b101b49df8aa
YanQinghao <woshiyanqinghao@gmail.com>
2019-09-20 11:23:12 +0800

    pure color mode

--- components/docker/SPDeepMatting.py
+++ components/docker/SPDeepMatting.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, print_function
 import os
 from suanpan.app import app
 from suanpan.screenshots import screenshots
-from suanpan.app.arguments import Int, Folder
+from suanpan.app.arguments import Int, Folder, ListOfInt
 import tensorflow as tf
 import numpy as np
 import imageio
@@ -15,6 +15,11 @@ from utils import composite4, rgba2rgb, get_all_files
 @app.input(Folder(key="inputData1"))
 @app.input(Folder(key="inputData2"))
 @app.param(Int(key="__gpu", default=0))
+@app.param(
+    ListOfInt(
+        key="bgColor", default=[255, 0, 0], help="255, 0, 0 255, 255, 255 67, 142, 219"
+    )
+)
 @app.output(Folder(key="outputData"))
 def SPDeepMatting(context):
 
@@ -57,107 +62,85 @@ def SPDeepMatting(context):
             final_alpha = 255 * final_alpha
             final_alpha = final_alpha.astype(np.uint8)
             screenshots.save(final_alpha)
-            # imageio.imwrite(os.path.join(args.outputData, rgb_pth), final_alpha)
+            # imageio.imwrite(os.path.join(args.outputData, os.path.split(rgb_pth)[1]), final_alpha)
             rgb_raw = imageio.imread(rgb_pth)
-            bg_pths = get_all_files(args.inputData2)
-            for bg_pth in bg_pths:
-                if os.path.splitext(bg_pth)[-1] not in [".png", ".jpg", ".jpeg"]:
-                    continue
-                bg_raw = imageio.imread(bg_pth)
-                if (
-                    bg_raw.shape[0] < rgb_raw.shape[0]
-                    or bg_raw.shape[1] < rgb_raw.shape[1]
-                ):
-                    rgb_raw = (
-                        np.array(
-                            Image.fromarray(rgb_raw.astype(np.uint8)).resize(
-                                (
-                                    bg_raw.shape[0],
-                                    int(
-                                        rgb_raw.shape[1]
-                                        * bg_raw.shape[0]
-                                        / rgb_raw.shape[0]
-                                    ),
+            if args.inputData2:
+                bg_pths = get_all_files(args.inputData2)
+                for bg_pth in bg_pths:
+                    if os.path.splitext(bg_pth)[-1] not in [".png", ".jpg", ".jpeg"]:
+                        continue
+                    bg_raw = imageio.imread(bg_pth)
+                    h_ratio = bg_raw.shape[0] / rgb_raw.shape[0]
+                    w_ratio = bg_raw.shape[1] / rgb_raw.shape[1]
+                    if h_ratio < 1 or w_ratio < 1:
+                        rgb_raw = (
+                            np.array(
+                                Image.fromarray(rgb_raw.astype(np.uint8)).resize(
+                                    (int(rgb_raw.shape[1] * h_ratio), bg_raw.shape[0])
+                                )
+                            )
+                            if h_ratio < w_ratio
+                            else np.array(
+                                Image.fromarray(rgb_raw.astype(np.uint8)).resize(
+                                    (bg_raw.shape[1], int(rgb_raw.shape[0] * w_ratio))
                                 )
                             )
                         )
-                        if bg_raw.shape[0] / rgb_raw.shape[0]
-                        < bg_raw.shape[1] / rgb_raw.shape[1]
-                        else np.array(
-                            Image.fromarray(rgb_raw.astype(np.uint8)).resize(
-                                (
-                                    int(
-                                        rgb_raw.shape[0]
-                                        * bg_raw.shape[1]
-                                        / rgb_raw.shape[1]
-                                    ),
-                                    bg_raw.shape[1],
+                        final_alpha = (
+                            np.array(
+                                Image.fromarray(final_alpha.astype(np.uint8)).resize(
+                                    (
+                                        int(final_alpha.shape[1] * h_ratio),
+                                        bg_raw.shape[0],
+                                    )
+                                )
+                            )
+                            if h_ratio < w_ratio
+                            else np.array(
+                                Image.fromarray(final_alpha.astype(np.uint8)).resize(
+                                    (
+                                        bg_raw.shape[1],
+                                        int(final_alpha.shape[0] * w_ratio),
+                                    )
                                 )
                             )
                         )
+                        origin_shape = final_alpha.shape
+                    im, bg = composite4(
+                        rgb_raw, bg_raw, final_alpha, origin_shape[1], origin_shape[0]
                     )
-                    final_alpha = (
-                        np.array(
-                            Image.fromarray(final_alpha.astype(np.uint8)).resize(
-                                (
-                                    bg_raw.shape[0],
-                                    int(
-                                        rgb_raw.shape[1]
-                                        * bg_raw.shape[0]
-                                        / rgb_raw.shape[0]
-                                    ),
-                                )
-                            )
-                        )
-                        if bg_raw.shape[0] / rgb_raw.shape[0]
-                        < bg_raw.shape[1] / rgb_raw.shape[1]
-                        else np.array(
-                            Image.fromarray(rgb_raw.astype(np.uint8)).resize(
-                                (
-                                    int(
-                                        rgb_raw.shape[0]
-                                        * bg_raw.shape[1]
-                                        / rgb_raw.shape[1]
-                                    ),
-                                    bg_raw.shape[1],
-                                )
-                            )
-                        )
-                    )
-                    origin_shape = (
-                        (
-                            int(rgb_raw.shape[1] * bg_raw.shape[0] / rgb_raw.shape[0]),
-                            bg_raw.shape[0],
-                        )
-                        if bg_raw.shape[0] / rgb_raw.shape[0]
-                        < bg_raw.shape[1] / rgb_raw.shape[1]
-                        else (
-                            bg_raw.shape[1],
-                            int(rgb_raw.shape[0] * bg_raw.shape[1] / rgb_raw.shape[1]),
-                        )
-                    )
-                im, bg = composite4(
-                    rgb_raw, bg_raw, final_alpha, origin_shape[1], origin_shape[0]
-                )
-                if not os.path.exists(
-                    os.path.join(
-                        args.outputData, os.path.split(bg_pth)[1].split(".")[0]
-                    )
-                ):
-                    os.makedirs(
+                    if not os.path.exists(
                         os.path.join(
                             args.outputData, os.path.split(bg_pth)[1].split(".")[0]
                         )
+                    ):
+                        os.makedirs(
+                            os.path.join(
+                                args.outputData, os.path.split(bg_pth)[1].split(".")[0]
+                            )
+                        )
+                    imageio.imwrite(
+                        os.path.join(
+                            args.outputData,
+                            os.path.split(bg_pth)[1].split(".")[0],
+                            os.path.split(rgb_pth)[1],
+                        ),
+                        im,
                     )
-                imageio.imwrite(
-                    os.path.join(
-                        args.outputData,
-                        os.path.split(bg_pth)[1].split(".")[0],
-                        os.path.split(rgb_pth)[1],
-                    ),
-                    im,
-                )
-                screenshots.save(bg)
-                # imageio.imwrite(os.path.join(args.outputData, "new_bg.png"), bg)
+                    screenshots.save(bg)
+                    # imageio.imwrite(os.path.join(args.outputData, "new_bg.png"), bg)
+            bgPure = np.ones(rgb_raw.shape)
+            for i, color in enumerate(args.bgColor):
+                bgPure[:, :, i] = bgPure[:, :, i] * color
+            im, bg = composite4(
+                rgb_raw, bgPure, final_alpha, origin_shape[1], origin_shape[0]
+            )
+            if not os.path.exists(os.path.join(args.outputData, "pure_color")):
+                os.makedirs(os.path.join(args.outputData, "pure_color"))
+            imageio.imwrite(
+                os.path.join(args.outputData, "pure_color", os.path.split(rgb_pth)[1]),
+                im,
+            )
+            # screenshots.save(bg)
 
     return args.outputData
